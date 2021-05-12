bfe8393f843a92cfa4a9f34cf9fa91aa8cb56a95
Andy Willis <github@awillis.fastmail.fm>
2021-05-12 10:56:55 +0100

    Updated

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const libraryTemp = require('./library/library.json').reduce((acc, c) => {
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
