fd25b912c108f37e8ebf598194a7c1bcd5a96d61
Andy Willis <awillis@fastmail.fm>
2022-07-01 20:31:45 +0100

    Updated

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path, { dirname } from 'path';
 import https from 'https';
+import http from 'http';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import DraftLog from 'draftlog';
@@ -15,16 +16,18 @@ const __dirname = dirname(__filename);
 const key = fs.readFileSync(`${__dirname}/auth/key.pem`);
 const cert = fs.readFileSync(`${__dirname}/auth/cert.pem`);
 
-const options = { key, cert };
+const serverOptions = { key, cert };
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.use(express.json());
 
 const port = process.env.PORT || 4000;
 
-const server = https.createServer(options, app);
+const server = process.env.NODE_ENV === 'production'
+  ? http.createServer(app)
+  : https.createServer(serverOptions, app);
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
