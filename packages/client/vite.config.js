5ea1169ad36f2b2dddfb4527de34eae247be8615
Andy Willis <awillis@fastmail.fm>
2022-07-29 16:20:59 +0100

    Updated

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -2,8 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import compress from 'vite-plugin-compression';
+import { splitVendorChunkPlugin } from 'vite'
+
 export default defineConfig({
-  plugins: [ react() ],
+  plugins: [ react(), compress(), splitVendorChunkPlugin() ],
   // server: {
   //   proxy: {
   //     '/message/add': {
