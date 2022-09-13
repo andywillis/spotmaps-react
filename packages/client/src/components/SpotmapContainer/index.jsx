7c5a4cc609521281b7d2a2765f0a9dd66c32d527
Andy Willis <awillis@fastmail.fm>
2022-09-13 18:54:28 +0100

    Updated to rgb2ase

--- packages/client/src/components/SpotmapContainer/index.jsx
+++ packages/client/src/components/SpotmapContainer/index.jsx
@@ -13,9 +13,17 @@ function SpotmapContainer(props) {
   const { state: { mainWidth } } = useContext(AppContext);
   const { data: { hexData, numberOfSpots, ...details } } = props;
 
+  function handleAseDownload(filename) {
+    console.log(filename);
+  }
+
   return (
     <section className={styles.spotmapContainer} style={{ width: mainWidth }}>
-      <Details mainWidth={mainWidth} data={details} />
+      <Details
+        mainWidth={mainWidth}
+        data={details}
+        handleAseDownload={handleAseDownload}
+      />
       <SpotmapCanvas
         mainWidth={mainWidth}
         numberOfSpots={numberOfSpots}
---
+++ packages/server/src/routes/ase.js
@@ -0,0 +1,11 @@
+import toSwatch from 'rgb2ase';
+
+export default async function library(library) {
+  return function (req, res) {
+    
+    const { filename } = req.params;
+    
+    res.download(file);
+  };
+
+}
