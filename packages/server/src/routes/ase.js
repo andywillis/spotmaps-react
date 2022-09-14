93924792ea0539373e5b1ba46e2431da5d57fd9d
Andy Willis <awillis@fastmail.fm>
2022-09-14 20:45:23 +0100

    Updated

--- packages/server/src/routes/ase.js
+++ packages/server/src/routes/ase.js
@@ -1,11 +1,13 @@
 import toSwatch from 'rgb2ase';
 
 export default async function library(library) {
+
   return function (req, res) {
     
     const { filename } = req.params;
     
     res.download(file);
+  
   };
 
 }
